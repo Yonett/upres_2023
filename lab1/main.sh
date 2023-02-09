e2d9f7ec6eefe6f1e26f9ec66360ac3066d2e8f9
yonett <yonett@mail.ru>
2023-02-09 12:12:52 +0700

    bash script done

--- lab1/main.sh
+++ lab1/main.sh
@@ -1,9 +1,34 @@
 # !/bin/bash
 
-ls=$(ls -lF $1)
-count=0
-for word in $ls
-do
-    if [  ]
-    echo ${word}
-done
+if [ $# -eq 1 ]
+then
+    if [ -d $1 ]
+    then
+        if [ ${1: -1} == "/" ]
+        then
+            path="$1*"
+        else
+            path="$1/*"
+        fi
+
+        for file in $path
+        do
+            if [ -d $file ]
+            then
+                echo $file
+            fi
+        done
+
+        for file in $path
+        do
+            if [ -f $file ]
+            then
+                echo $(stat --format="%n %s %y %h" $file)
+            fi
+        done
+    else
+        echo "$1 is not a directory"
+    fi
+else
+    echo "Wrong number of arguments (expected 1, given $#)"
+fi
