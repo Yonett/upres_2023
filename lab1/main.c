2c25d84970b9ecdee3924a3e758741358bc59661
yonett <yonett@mail.ru>
2023-02-15 21:30:32 +0700

    program in c

--- lab1/main.c
+++ lab1/main.c
@@ -1,7 +1,68 @@
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("Hello World");
+    DIR *d;
+    char cwd[512];
+    char input[25];
+    struct dirent *filedir;
+    if(argv[1] == NULL)
+    {
+       printf("Error! Directory no specified!\n");
+       exit(0);   
+    }   
+    d = opendir(argv[1]);
+
+    if(!d)
+    {
+       printf("Error! No such directory!\n");
+       exit(0);
+    }
+
+    printf("\nStart directory - (/%s)\n", argv[1]);
+    
+    while((filedir = readdir(d)) != NULL)
+    {   
+        if(filedir->d_type == DT_DIR && filedir->d_name[0] != '.')
+            {
+               printf("%s\n", filedir->d_name);
+            }
+    }
+    closedir(d);
+
+    struct dirent *filenodir;
+    if(argv[1] == NULL)
+    {
+       printf("Error! Directory no specified!\n");
+       exit(0);
+    }
+    d = opendir(argv[1]);
+
+    if(!d)
+    {
+       printf("Error! No such directory!\n");
+       exit(0);
+    }
+    while((filenodir = readdir(d)) != NULL)
+    {
+        if(filenodir->d_type != DT_DIR)
+            {
+               printf("%s ", filenodir->d_name);
+               struct stat buff;
+               stat(filenodir, &buff);
+               time(&buff.st_mtime);
+               printf("Размер - %ld bytes, Время -  %s, Кол-во ссылок - %ld\n",  buff.st_size, ctime(&buff.st_mtime), buff.st_nlink);
+            }
+    }
+    closedir(d);
     return 0;
+
 }
