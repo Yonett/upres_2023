0c2dff75a766a5d76fded3ef3b403541d19fbfe9
yonett <yonett@mail.ru>
2023-03-15 21:33:49 +0700

    sync with wait() func and comments

--- lab2/factorial.c
+++ lab2/factorial.c
@@ -3,19 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-#include <time.h>
 
 /**
  * Функция считает факториал целого числа.
  * @param n Число, факториал которого нужно рассчитать.
  * @return Значение факториала числа.
  **/
-int factorial (int n)
+int factorial(int n)
 {
     int i, result = 1;
     for (i = 2; i <= n; ++i)
@@ -25,48 +23,46 @@ int factorial (int n)
 
 int main(int argc, char** argv)
 {
-    if (argc == 1)
+    if (argc == 1) //Для расчёта n!
     {
         int n, fd, result;
         char *endptr;
 
-        n = strtol(argv[0], &endptr, 10);
+        n = strtol(argv[0], &endptr, 10); //Конвертирование строки с аргументом к типу integer.
         if (*endptr != '\0')
         {
             fprintf(stderr, "Error! ProcessN cannot convert fisrt argument (%s) to integer.\n", argv[0]);
-            truncate("data", 0);
             return(EXIT_FAILURE);
         }
 
-        result = factorial(n);
-        fd = open("data", O_TRUNC | O_WRONLY);
-        write(fd, &result, sizeof(int));
-        close(fd);
+        result = factorial(n); //Расчёт n!
+        fd = open("data", O_TRUNC | O_WRONLY); //Открываем файл для записи с изменением длины файла до 0
+        write(fd, &result, sizeof(int)); //Записываем в файл значение n!
+        close(fd); //Закрываем файл.
     }
-    else if (argc == 2)
+    else if (argc == 2) //Для расчёта (n - m)!
     {
         int n, m, fd, result;
         char *endptr;
 
-        n = strtol(argv[0], &endptr, 10);
+        n = strtol(argv[0], &endptr, 10); //Конвертирование строки с первым аргументом к типу integer.
         if (*endptr != '\0')
         {
             fprintf(stderr, "Error! ProcessNM cannot convert fisrt argument (%s) to integer.\n", argv[0]);
             exit(EXIT_FAILURE);
         }
 
-        m = strtol(argv[1], &endptr, 10);
+        m = strtol(argv[1], &endptr, 10); //Конвертирование строки со вторым аргументом к типу integer.
         if (*endptr != '\0')
         {
             fprintf(stderr, "Error! ProcessNM cannot convert second argument (%s) to integer.\n", argv[1]);
-            truncate("data", 0);
             exit(EXIT_FAILURE);
         }
 
-        result = factorial(n - m);
-        fd = open("data", O_WRONLY | O_APPEND);
-        write(fd, &result, sizeof(int));
-        close(fd);
+        result = factorial(n - m); //Расчёт (n - m)!
+        fd = open("data", O_WRONLY | O_APPEND); //Открываем файл для записи в режиме добавления.
+        write(fd, &result, sizeof(int)); //Записываем в файл значение (n - m)!
+        close(fd); //Закрываем файл.
     }
     return 0;
 }
--- lab2/main.c
+++ lab2/main.c
@@ -7,7 +7,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
-#include <time.h>
 
 int main(int argc, char** argv)
 {
@@ -17,49 +16,57 @@ int main(int argc, char** argv)
       exit(EXIT_FAILURE);
     }
 
-    pid_t pidNM = fork();
+    pid_t pidNM = fork(); //Процесс, который будет считать (n - m)!
  
-    if (pidNM == -1)
-        fprintf(stderr, "Main process unable to fork\n");
-    else if (pidNM > 0)
+    if (pidNM == -1) //Если не удалось создать процесс.
+        fprintf(stderr, "Main process unable to fork.\n");
+    else if (pidNM > 0) //Родительский процесс. 
     {
         int status;
-        wait(&status);
+        wait(&status); //Ожидает завершения процесса NM.
+        if (status != EXIT_SUCCESS) //Если процесс NM завершился с ошибкой.
+            exit(EXIT_FAILURE);
+
         int fd, nFactorial, nmFactorial;
 
-        fd = open("data", O_RDONLY);
-        if (read(fd, &nFactorial, sizeof(int)) != sizeof(int) || read(fd, &nmFactorial, sizeof(int)) != sizeof(int))
-            {
-                close(fd);
-                exit(EXIT_FAILURE);
-            }
-        close(fd);
+        fd = open("data", O_RDONLY); //Открываем файл с данными для чтения.
+        if (read(fd, &nFactorial, sizeof(int)) != sizeof(int) ||
+            read(fd, &nmFactorial, sizeof(int)) != sizeof(int)) //Проверяем, чтобы считалось нужное количество байт (два раза по int).
+        {
+            close(fd); //Закрываем файл.
+            exit(EXIT_FAILURE); 
+        }
+        close(fd); //Закрываем файл.
 
-        printf("Number of permutations without repetitions: %d\n", (nFactorial / nmFactorial));
+        printf("Number of permutations without repetitions: %d.\n", (nFactorial / nmFactorial)); //Вывод результата
     }
-    else
+    else //Процесс NM
     {
-        pid_t pidN = fork();
-        if (pidN == -1)
-            fprintf(stderr, "Process NM unable to fork\n");
-        else if (pidN > 0)
+        pid_t pidN = fork(); //Процесс, который будет считать n!
+        if (pidN == -1) //Если не удалось создать процесс.
+            fprintf(stderr, "Process NM unable to fork.\n");
+        else if (pidN > 0) //Родительский процесс.
         {
             int status;
-            wait(&status);
-            if (execl("factorial.o", argv[1], argv[2], NULL) == -1)
+            wait(&status); //Ожидает завершения процесса N.
+            if (status != EXIT_SUCCESS) //Если процесс N завершился с ошибкой.
+                exit(EXIT_FAILURE);
+            if (execl("factorial.o", argv[1], argv[2], NULL) == -1) //Запуск программы для расчёта (n - m)!
             {
-                fprintf(stderr, "Process NM unable to exec facrotial.o\n");
+                fprintf(stderr, "Process NM unable to exec factorial.o (%s).\n", strerror(errno));
                 exit(EXIT_FAILURE);
             }
             exit(EXIT_SUCCESS);
         }
-        else
-            if (execl("factorial.o", argv[1], NULL) == -1)
+        else //Процесс N.
+        {
+            if (execl("factorial.o", argv[1], NULL) == -1) //Запуск программы для расчёта n!
             {
-                fprintf(stderr, "Process N unable to exec facrotial.o\n");
+                fprintf(stderr, "Process N unable to exec factorial.o (%s).\n", strerror(errno));
                 exit(EXIT_FAILURE);
             }
             exit(EXIT_SUCCESS);
+        }
     }
     return 0;
 }
